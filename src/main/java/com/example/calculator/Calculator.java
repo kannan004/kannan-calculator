5bb7c849b00c40a53a2f07db5380fd8aaa3ce33d
kannan004 <kannan820408@gmail.com>
2026-06-29 13:49:30 +0530

    Update Calculator.java

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -2,14 +2,10 @@ package com.example.calculator;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a - b;
-    }
-
-    public int subtract(int a, int b) {
         return a + b;
     }
 
-    public int Add(int a, int b) {
+    public int subtract(int a, int b) {
         return a + b;
     }
 
